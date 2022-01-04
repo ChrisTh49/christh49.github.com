2f50f48987b219808ea3e435272f05492e27cbd1
ChrisThDev <christopher.rincon@awkbit.com>
2022-01-04 16:55:12 -0300

    test

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        width="100%"
+        height="1200px"
+        src="https://pmi-project-map-it.127.0.0.1.xip.io/christhokidoki/map?map=61d32d75b0cc740c6f49bc9b"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
